2f64346201e4148ff2d81e1e3febb5bb6d8eba79
sinchir0 <dekunattou@gmail.com>
2025-09-10 23:35:34 +0000

    add

--- do_py.sh
+++ do_py.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-uv run python exp/exp001/train.py
-uv run python exp/exp001/evaluate.py
+uv run python exp/exp001_cls/train.py
+uv run python exp/exp001_cls/evaluate.py
 
 gcloud compute instances stop saito-gpu-map-dev --zone=us-central1-b
--- exp/exp001_cls/evaluate.py
+++ exp/exp001_cls/evaluate.py
@@ -4,22 +4,26 @@ from vllm import LLM, SamplingParams
 from transformers import AutoTokenizer
 import torch
 import pandas as pd
-from logits_processor_zoo.vllm import MultipleChoiceLogitsProcessor
 from pathlib import Path
 
 # ref: https://www.kaggle.com/code/aerdem4/eedi-qwen32b-vllm-with-logits-processor-zoo
+# ref: 
 DATA_PATH = Path("data")
-OUT_DIR = "outputs/exp001_cls/20250907230303"
+OUT_DIR = "outputs/exp001_cls/20250907233056"
 MAX_LEN = 256
 SEED = 42
-
+DEBUG = True
 if __name__ == "__main__":
     with open(f"{OUT_DIR}/all_completions.json", "r", encoding="utf-8") as f:
         all_completions = json.load(f)
-    
+
     val_df = pd.read_csv(f"{OUT_DIR}/val_df.csv")
-    val_df = val_df[:10]
+    if DEBUG:
+        val_df = val_df[:10]
     tokenizer = AutoTokenizer.from_pretrained(OUT_DIR, trust_remote_code=True)
+
+    allowed_token_ids = [tokenizer.encode(str(i), add_special_tokens=False)[0] for i in all_completions]
+
     vllm_model = LLM(
         model=OUT_DIR,  # 保存したモデルパスを指定
         trust_remote_code=True,
@@ -35,13 +39,16 @@ if __name__ == "__main__":
         top_p=1,  # greedy
         top_k=-1,  # greedy
         max_tokens=1,
+        logprobs=3, # MAP@3だから
         stop_token_ids=[tokenizer.eos_token_id],
-        logits_processors=[
-            MultipleChoiceLogitsProcessor(
-                tokenizer,
-                choices=all_completions
-            )
-        ]
+        allowed_token_ids=allowed_token_ids
+        # NOTE: MultipleChoiceLogitsProcessorは、vLLM のバージョン 0.10.1.1 では対応していない。
+        # logits_processors=[
+        #     MultipleChoiceLogitsProcessor(
+        #         tokenizer,
+        #         choices=all_completions
+        #     )
+        # ]
     )
 
     # val_df全体に対して推論実行
@@ -49,12 +56,12 @@ if __name__ == "__main__":
     prompts = val_df["prompt"].tolist()
     outputs = vllm_model.generate(prompts, sampling_params)
 
-    # 結果を保存
     predictions = []
     for output in outputs:
-        generated_text = output.outputs[0].text.strip()
-        predictions.append(generated_text)
+        prediction = [out.decoded_token for out in output.outputs[0].logprobs[0].values()]
+        predictions.append(prediction)
 
+    # 結果を保存
     val_df["prediction"] = predictions
 
     # 結果の一部を表示
@@ -67,3 +74,5 @@ if __name__ == "__main__":
     # 結果をCSVに保存
     val_df[["prompt", "completion", "prediction"]].to_csv(f"{OUT_DIR}/validation_results.csv", index=False)
     print(f"\n推論結果を {OUT_DIR}/validation_results.csv に保存しました")
+
+    # TODO: MAP@3の計算を追加する、計算方法から確認する
